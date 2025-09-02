ac937b525f714053a084842c4ec43f1d9be30657
Eduardo <edu_class@yahoo.com.br>
2025-09-02 16:56:04 -0400

    commit inicial

---
+++ src/main/java/com/edu/ProjetoComprovante/ProjetoComprovantesApplication.java
@@ -0,0 +1,13 @@
+package com.edu.ProjetoComprovante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoComprovantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoComprovantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/ProjetoComprovante/ProjetoComprovantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ProjetoComprovante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoComprovantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
